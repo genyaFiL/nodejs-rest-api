5427ff24d64c06d09f53bb8715cd8774cb6cc7fe
genyaFiL <genyafil@gmail.com>
2023-09-04 20:15:47 +0300

    del commit

--- models/contacts.js
+++ models/contacts.js
@@ -58,23 +58,3 @@ export default {
   updateContact,
   removeContact,
 };
-
-// // const fs = require('fs/promises')
-
-// const listContacts = async () => {}
-
-// const getContactById = async (contactId) => {}
-
-// const removeContact = async (contactId) => {}
-
-// const addContact = async (body) => {}
-
-// const updateContact = async (contactId, body) => {}
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// }
