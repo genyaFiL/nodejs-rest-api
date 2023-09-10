9a18fe1e27041bb95f8f3f873a8cef2a5de99674
genyaFiL <genyafil@gmail.com>
2023-09-10 15:04:36 +0300

    add Environment Variables

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+//genyafil
+//
+//bKUKyIZjdynnIAxI
+const DB_HOST =
+  "mongodb+srv://genyfil:bKUKyIZjdynnIAxI@cluster0.6stikgv.mongodb.net/my-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
