af9fb719ea67ab711fad254e63093eb3d8e29060
genyaFiL <genyafil@gmail.com>
2023-09-12 21:42:11 +0300

    cleare code

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-// import dotenv from "dotenv";
 import "dotenv/config";
 import contactsRouter from "./routes/api/contacts-router.js";
-
-// dotenv.config();
 
 const app = express();
 
